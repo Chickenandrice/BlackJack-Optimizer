0e4e7104cd8278e1943031696660bb70d21bfda2
Jchen9896 <jchen9896@vt.edu>
2024-07-21 19:21:03 -0400

    added basic structure

--- simulate_game.py
+++ simulate_game.py
@@ -1,1 +1,14 @@
+import dealing_cards as dc 
+import strategies.default as df
+import strategies.strategies as st
+import tracking.player as tp
+
 def simulate(number_trials): 
+    while number_trials > 0: 
+        
+
+
+        number_trials -= 1
+    
+
+simulate(5)
