3bb0cfdf1e45e2efe46954ceca6488b8f55c0ff4
Jchen9896 <jchen9896@vt.edu>
2024-07-21 19:23:06 -0400

    added methods for blackjack hand

--- tracking/player.py
+++ tracking/player.py
@@ -10,16 +10,24 @@ def sql_command(sql, db, val = None):
     else: 
         cursor.execute(sql)
     connection.commit()
+
+class Hand: 
+    def __init__(self):
+        self.hand = []
+    def draw_card(self, card): 
+        self.hand.append(card) 
+
         
 # holds the stats for a particular simulation 
 class Player: 
     # wlp stands for win loss push 
-    def __init__(self, name, balance, entry, hand, wlp):
+    def __init__(self, name, balance, entry, wlp):
         self.name = name 
         self.balance = balance 
         self.entry = entry
-        self.hand = hand
+        self.hand = [Hand()]
         self.wlp = wlp
+
 
     def add(self, amount):
         self.balance += amount
@@ -28,10 +36,22 @@ class Player:
         db = self.name + ".db"
 
         sql_command(f'CREATE TABLE IF NOT EXISTS {self.name} (balance INTEGER, entry INTEGER, WLP TEXT)', db)
-        sql_command(f'INSERT INTO {self.name} (balance, entry, WLP) VALUES (?, ?, ?)', db, (self.balance, "0", 'N/A'))
+        sql_command(f'INSERT INTO {self.name} (balance, entry, WLP) VALUES (?, ?, ?)', db, (self.balance, "0", 'N/A')) 
         sqlite3.connect(db).close()
     
-    def bet(self, amount):
+    def split(): 
+        pass
+
+    def stand(): 
+        pass
+    def hit(self): 
+        self.hand.append(Dealer.deal_card())
+
+    def insurance(): 
+        if Dealer.hand:
+            pass
+    
+    def final_bet(self, amount, insurance=None): 
         db = self.name + ".db"
         outcome = ""
         if self.balance < amount:
@@ -43,7 +63,8 @@ class Player:
             self.balance -= amount
             outcome = "loss"
         else: 
-            outcome = "pass"
+            outcome = "push"
+        
         if amount == -1: 
             outcome = "N/A"
         sql_command(f'INSERT INTO {self.name} (balance, entry, WLP) VALUES (?, ?, ?)', db, (self.balance, amount, outcome))
