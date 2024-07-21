033eed124b4779f95deca58e80245b2f94a59cba
Jchen9896 <jchen9896@vt.edu>
2024-07-21 00:05:25 -0400

    revised structure and put methods into a dealer class for organization

--- dealing_cards/dealer.py
+++ dealing_cards/dealer.py
@@ -1,11 +1,25 @@
 import cards
 import random
 
-def new_deck(num_decks):
-    play_cards = []
-    for card in cards.deck: 
-        play_cards.append(card)
-    return play_cards*num_decks
+class Dealer():
+    def __init__(self, hand):
+        self.hand = hand
 
-def deal_cards(deck):
-    return deck.pop(random.randint(0, len(deck)))
+    def new_deck(num_decks):
+        play_cards = []
+        for card in cards.deck: 
+            play_cards.append(card)
+
+        return play_cards*num_decks
+
+    def deal_card(deck):
+        return deck.pop(random.randint(0, len(deck)))
+
+    def compare_hands(self, player_hand): 
+        if self.hand > player_hand: 
+            return "loss"
+        elif self.hand == player_hand: 
+            return "push"
+        else:
+            return "win"
+    
