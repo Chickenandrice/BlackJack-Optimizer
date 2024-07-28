19d478a5152f65840ba393a382a00fc00d34f060
Jchen9896 <jchen9896@gmail.com>
2024-07-27 21:16:57 -0400

    added documentation

--- components/cards.py
+++ components/cards.py
@@ -1,13 +1,25 @@
 
-def new_deck(num_decks):
+def new_deck(num_decks: int) -> list[tuple[str, str]]:
+    """
+    gets a list of tuples that represents card in a playing deck. 
+
+    Parameters: 
+
+    num_decks (int): The number of decks that should be in a game
+
+    Returns:
+
+    list[tuple[str,str]]: a list of tuples, which represent playing cards
+    """
     play_cards = []
     for card in deck: 
         play_cards.append(card)
 
     return play_cards*num_decks
 
+# list of tuples that represent cards in a standard 52 card deck 
 deck = [
-("2","Clubs"),
+("2","Clubs"), 
 ("2","Diamonds"),
 ("2","Hearts"),
 ("2","Spades"),
@@ -60,3 +72,24 @@ deck = [
 ("Ace","Hearts"),
 ("Ace","Spades"),
 ]
+
+
+"""
+test cases 
+
+def new_deck2(num_decks, decks):
+    play_cards = []
+    for card in decks: 
+        play_cards.append(card)
+
+    return play_cards*num_decks
+
+deck2 = [("Ace","Clubs"),("7","Diamonds")]
+deck3 = [("7","Diamonds"),("Ace","Clubs")]
+deck4 = [("4","Diamonds"),("Ace","Clubs"),("7", "Clubs")]
+deck5 = [('4', 'Hearts'), ('Ace', 'Diamonds'), ('6', 'Hearts')]
+deck6 = [('Ace', 'Diamonds'), ('10', 'Diamonds'), ('9', 'Diamonds')]
+deck7 = [('10', 'Hearts'), ('3', 'Hearts'), ('3', 'Spades'), ('Ace', 'Hearts')]
+deck8 = [('Ace', 'Spades'), ('3', 'Clubs'), ('9', 'Spades'), ('Jack', 'Diamonds'), ('King', 'Spades'), ('8', 'Spades')]
+
+"""
