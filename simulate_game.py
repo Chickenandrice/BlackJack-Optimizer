37a10c1ef88d4ad340b73a5e0259dfbff0ad2e69
Jchen9896 <jchen9896@gmail.com>
2024-07-28 00:09:38 -0400

    wrote placeholder simulations for testing

--- simulate_game.py
+++ simulate_game.py
@@ -1,14 +1,37 @@
-import dealing_cards as dc 
 import strategies.default as df
 import strategies.strategies as st
-import tracking.player as tp
+from components.player import Player
+from components.cards import new_deck
+from components.dealer import Dealer
 
-def simulate(number_trials): 
+
+def simulate_three_cards_no_split(number_trials, player_name, player_balance): 
+    dlr = Dealer()
+    Plyer = Player(player_name, player_balance) 
     while number_trials > 0: 
-        
-
-
+        deck = new_deck(6)
+        Plyer.bet(100)
+        dealer_hand, dealer_total = dlr.deal_self(deck)
+        Plyer.initial_deal(1, deck, dlr)
+        Plyer.split(1, deck, dlr)
+        Plyer.hit(1, deck, dlr)
+        Plyer.check_bet(dealer_total)
+        Plyer.new_hand()
+        dlr.reset() 
         number_trials -= 1
-    
 
-simulate(5)
+def simulate_two_cards_no_split(number_trials, player_name, player_balance): 
+    dlr = Dealer()
+    Plyer = Player(player_name, player_balance) 
+    while number_trials > 0:
+        deck = new_deck(6)
+        Plyer.bet(100)
+        hand, total = dlr.deal_self(deck)
+        Plyer.hit(1, deck, dlr)
+        Plyer.hit(1, deck, dlr)
+        Plyer.check_bet(total)
+        Plyer.new_hand()
+        dlr.reset() 
+        number_trials -= 1
+
+simulate_three_cards_no_split(100, "mark", 10000)
