4f95d4f78953c3722356b13fcdfa2091cede0042
Jchen9896 <jchen9896@gmail.com>
2024-07-31 01:49:32 -0400

    wrote two methods, one that simulates a default basic method and another that simulates a stragedy that should be created in the strategies.py file

--- simulate_game.py
+++ simulate_game.py
@@ -1,37 +1,72 @@
 import strategies.default as df
 import strategies.strategies as st
 from components.player import Player
-from components.cards import new_deck
 from components.dealer import Dealer
+from plot_data import plot_trial_balance
 
 
-def simulate_three_cards_no_split(number_trials, player_name, player_balance): 
+def simulate_default_trials(number_trials: int, player_name: str, player_balance: int, bet_amount: int):
+    """ 
+    This method simulates a basic default strategy over many different trials, creates a database, and plots the results 
+
+    Parameters: 
+
+    number_trials (int): the number of individual games that will be simulated at one time. 
+    player_name (str): the name of the simulation 
+    player_balance (int): the starting balance of the simulation 
+    bet_amount (int): the starting bet amount, which will remain constant unless a strategy requires it to change 
+
+    """
+    # sets up dealer 
     dlr = Dealer()
-    Plyer = Player(player_name, player_balance) 
+    
+    # sets up player 
+    player = Player(player_name, player_balance) 
+
+    i = 1
     while number_trials > 0: 
-        deck = new_deck(6)
-        Plyer.bet(100)
-        dealer_hand, dealer_total = dlr.deal_self(deck)
-        Plyer.initial_deal(1, deck, dlr)
-        Plyer.split(1, deck, dlr)
-        Plyer.hit(1, deck, dlr)
-        Plyer.check_bet(dealer_total)
-        Plyer.new_hand()
-        dlr.reset() 
+        print(f'\nsimulation # {i}')
+        df.simulate_default(player, dlr, bet_amount)
         number_trials -= 1
+        i += 1
+    
+    # this line plots the data 
+    plot_trial_balance(player_name) 
 
-def simulate_two_cards_no_split(number_trials, player_name, player_balance): 
+
+def simulate_your_strategy(number_trials, player_name, player_balance, bet_amount):
+    """ 
+    This method simulates a basic default strategy over many different trials, creates a database, and plots the results 
+
+    Parameters: 
+
+    number_trials (int): the number of individual games that will be simulated at one time. 
+    player_name (str): the name of the simulation 
+    player_balance (int): the starting balance of the simulation 
+    bet_amount (int): the starting bet amount, which will remain constant unless a strategy requires it to change 
+
+    """
+    # sets up dealer 
     dlr = Dealer()
-    Plyer = Player(player_name, player_balance) 
-    while number_trials > 0:
-        deck = new_deck(6)
-        Plyer.bet(100)
-        hand, total = dlr.deal_self(deck)
-        Plyer.hit(1, deck, dlr)
-        Plyer.hit(1, deck, dlr)
-        Plyer.check_bet(total)
-        Plyer.new_hand()
-        dlr.reset() 
-        number_trials -= 1
+    
+    # sets up player 
+    player = Player(player_name, player_balance) 
 
-simulate_three_cards_no_split(100, "mark", 10000)
+    i = 1
+    while number_trials > 0: 
+        print(f'\nsimulation # {i}')
+        st.simulate_your_strategy(player, dlr, bet_amount) # THIS IS WHERE YOUR STRATEGY IS IMPLEMENTED**
+        number_trials -= 1
+        i += 1
+    
+    # this line plots the data 
+    plot_trial_balance(player_name) 
+
+
+
+
+# sample simulation run
+simulate_default_trials(1000, "test", 10000, 100) 
+
+# RUNNING YOUR STRATEGY**
+#simulate_your_strategy(1000, "custom_strategy", 1000, 100)
